d6d9c026b825ad3cc0ea8ef614962221a5afb4dc
Matheus Henrique Pedroso Viana <matheus_pviana@hotmail.com>
2022-02-07 21:08:21 -0300

    Changes

--- script.js
+++ script.js
@@ -10,8 +10,21 @@ for (var i=0; themeDots.length > i; i++){
     }) 
 }
 
-function setTheme(mode {
+function setTheme(mode) {
+
     if(mode == 'light'){
-        
+        document.getElementById('theme-style').href = 'light.css';
     }
-})
+
+    if(mode == 'blue'){
+        document.getElementById('theme-style').href = 'blue.css';
+    }
+
+    if(mode == 'green'){
+        document.getElementById('theme-style').href = 'green.css';
+    }
+
+    if(mode == 'purple'){
+        document.getElementById('theme-style').href = 'purple.css';
+    }
+}
