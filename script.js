93d43410a38d4e602962d213777b549eefb271c6
Matheus Henrique Pedroso Viana <matheus_pviana@hotmail.com>
2022-02-08 18:01:49 -0300

    No nav bar

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
 console.log('Funcionando')
 
+let theme = localStorage.getItem('theme')
+
+if (theme == null) {
+    setTheme('light')
+} else {
+    setTheme(theme)
+}
+
 let themeDots = document.getElementsByClassName("theme-dot")
 
-for (var i=0; themeDots.length > i; i++){
-    themeDots[i].addEventListener('click', function(){
+for (var i = 0; themeDots.length > i; i++) {
+    themeDots[i].addEventListener('click', function () {
         let mode = this.dataset.mode
         console.log("Evento clicado:", mode)
         setTheme(mode)
-    }) 
+    })
 }
 
 function setTheme(mode) {
 
-    if(mode == 'light'){
+    if (mode == 'light') {
         document.getElementById('theme-style').href = 'light.css';
     }
 
-    if(mode == 'blue'){
+    if (mode == 'blue') {
         document.getElementById('theme-style').href = 'blue.css';
     }
 
-    if(mode == 'green'){
+    if (mode == 'green') {
         document.getElementById('theme-style').href = 'green.css';
     }
 
-    if(mode == 'purple'){
+    if (mode == 'purple') {
         document.getElementById('theme-style').href = 'purple.css';
     }
+
+    localStorage.setItem('theme', mode)
+
 }
+
